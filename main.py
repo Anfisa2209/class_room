5544c4e6fc3791b60b3cc96836d361b5718971a4
Anfisa <anfis.igorevna@gmail.com>
2025-02-27 21:27:59 +0300

    Часть №9

--- main.py
+++ main.py
@@ -16,12 +16,13 @@ class MyWidget(QMainWindow):
         self.map_ll = [37.687874, 55.765290]
         self.z = 5
         self.theme = 'light'
-        self.pt = ''
+        self.pt = self.post_index = self.full_address = ''
         self.refresh_map()
         self.change_theme_btn.clicked.connect(self.change_theme)
         self.find_btn.clicked.connect(self.find_address)
         self.delete_point_btn.clicked.connect(self.delete_point)
         self.address.returnPressed.connect(self.find_address)
+        self.index.stateChanged.connect(self.add_post_index)
         self.setFocusPolicy(Qt.FocusPolicy.StrongFocus)
 
     def refresh_map(self):
@@ -39,8 +40,13 @@ class MyWidget(QMainWindow):
         self.change_theme_btn.setText('Темная тема' if self.theme == 'light' else 'Светлая тема')
         self.refresh_map()
 
+    def add_post_index(self):
+        if self.full_address:
+            text = f"{self.full_address}, {self.post_index}" if self.index.isChecked() else self.full_address
+            self.full_address_lbl.setText(text)
+
     def delete_point(self):
-        self.pt = ''
+        self.pt = self.post_index = ''
         self.full_address_lbl.setText('')
         self.refresh_map()
 
@@ -52,13 +58,19 @@ class MyWidget(QMainWindow):
             return
         try:
             toponym = get_object(address_text)
-            full_address = toponym["metaDataProperty"]["GeocoderMetaData"]["Address"]["formatted"]
+            self.full_address = toponym["metaDataProperty"]["GeocoderMetaData"]["Address"]["formatted"]
+            try:
+                self.post_index = toponym['metaDataProperty']['GeocoderMetaData']['Address']['postal_code']
+            except KeyError:
+                self.post_index = '(нет почтового индекса)'
             self.map_ll, spn = get_ll_spn(toponym)
             self.map_ll = list(map(float, self.map_ll.split(',')))
             self.z = 10
             self.pt = ",".join(map(str, self.map_ll))
-            self.full_address_lbl.setText(full_address)
-        except:
+            text = f"{self.full_address}, {self.post_index}" if self.index.isChecked() else self.full_address
+            self.full_address_lbl.setText(text)
+
+        except Exception:
             self.error_lbl.setText('Ошибка! Такого адреса не существует.')
         self.refresh_map()
 
